ae71c19e895b1b74ece8dd8927618bf2836707eb
Martin Neshev <martinneshevsr@gmail.com>
2023-05-22 22:54:36 +0300

    App makes API calls every 60 seconds in order to update coins data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,20 +6,31 @@ import styles from "./Home.module.css";
 const Home = () => {
     const [allCoins, setAllCoins] = useState([]);
 
+    //initial API call of coins data
     useEffect(() => {
-        getFirstTwelveCoinsByVolume()
-            .then((res) => {
-                setAllCoins(res.data);
-            })
-            .catch((error) => {
-                //handle error here
+        const getApiData = async () => {
+            try {
+                const coinsData = await getFirstTwelveCoinsByVolume();
+                setAllCoins(coinsData.data);
+            } catch (error) {
                 console.log(error);
-            });
+            }
+        };
+        getApiData();
+
+
+    //update of API data every 60 seconds 
+        const interval = setInterval(() => {
+            getApiData();
+        }, 60000);
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
 
     const tableTotalStyle = {
-      justifyContent: "flex-start",
-  };
+        justifyContent: "flex-start",
+    };
 
     return (
         <div className={styles["home-container"]}>
@@ -31,9 +42,11 @@ const Home = () => {
                     <p className={styles["th-td"]}>24h %</p>
                     <p className={styles["th-td"]}>Market Cap</p>
                     <p className={styles["th-td"]}>Volume 24h</p>
-                    <p className={styles["th-td"]} style={tableTotalStyle}>Circulating Supply</p>
+                    <p className={styles["th-td"]} style={tableTotalStyle}>
+                        Circulating Supply
+                    </p>
                 </div>
-                
+
                 {allCoins.map((coin, index) => (
                     <Coin key={coin.id} coin={coin} index={index} />
                 ))}
